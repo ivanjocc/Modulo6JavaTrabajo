0cf8f18a926147bc8652996a4da92b13e126c489
Iván <ivanjose_04@hotmail.com>
2023-05-28 22:29:10 -0400

    first commit

---
+++ src/CocheCRUD.java
@@ -0,0 +1,6 @@
+// Interfaz CocheCRUD
+public interface CocheCRUD {
+    void save();
+    void findAll();
+    void delete();
+}
---
+++ src/CocheCRUDImpl.java
@@ -0,0 +1,17 @@
+// Implementación CocheCRUDImpl
+public class CocheCRUDImpl implements CocheCRUD {
+    @Override
+    public void save() {
+        System.out.println("Método save()");
+    }
+
+    @Override
+    public void findAll() {
+        System.out.println("Método findAll()");
+    }
+
+    @Override
+    public void delete() {
+        System.out.println("Método delete()");
+    }
+}
